7f18f21f4db166677258757f75d7f1e7839c4255
Ayush <agrawalayush07bansal@gmail.com>
2025-07-30 10:45:03 +0530

    day1

---
+++ Day1_ProgrammingElements.java
@@ -0,0 +1,5 @@
+public class Day1_ProgrammingElements {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! Welcome to Day 1 of Programming Elements.");
+    }
+}
